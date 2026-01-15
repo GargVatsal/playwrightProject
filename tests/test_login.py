6823e447012903ddce2dfbcce980ab9a2a8eceb1
GargVatsal <vatsalgarg1997@gmail.com>
2026-01-15 23:28:59 +0530

    Updated test case for Sauce Labs Demo web site

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,10 @@
+from playwright.sync_api import expect
 
 def test_login(context):
     page = context.new_page()
-    page.goto("https://google.com")
-    assert page.title() == "Google"
+    page.goto("https://www.saucedemo.com/")
+    page.get_by_text("Swag Labs").is_visible()
+    page.get_by_role("textbox", name="Username").fill("standard_user")
+    page.get_by_role("textbox", name="Password").fill("secret_sauce")
+    page.get_by_role("button", name= "Login").click()
+    expect(page.get_by_text("Products")).to_be_visible()
